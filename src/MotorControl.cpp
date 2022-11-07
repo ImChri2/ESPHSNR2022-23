4b170517fa194a2b08ec11184a1bcd381489d0e5
ImChri2 <chris@chris-industries.de>
2022-11-07 22:11:48 +0100

    added Commentary to main and Motorcontrol

--- src/MotorControl.cpp
+++ src/MotorControl.cpp
@@ -2,27 +2,28 @@
 
 int veloctiy = 190;
 
+//motor stop
 void motor_stop(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, 0);
   analogWrite(left_b, 0);
   analogWrite(right_a, 0);
   analogWrite(right_b, 0);
 }
-
+//motor left
 void motor_left(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, 0);
   analogWrite(left_b, veloctiy);
   analogWrite(right_a, veloctiy);
   analogWrite(right_b, 0);
 }
-
+//motor right
 void motor_right(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, veloctiy);
   analogWrite(left_b, 0);
   analogWrite(right_a, 0);
   analogWrite(right_b, veloctiy);
 }
-
+// motor forward
 void motor_forward(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, veloctiy);
   analogWrite(left_b, 0);
@@ -33,7 +34,7 @@ void motor_forward(int left_a, int left_b, int right_a, int right_b) {
 void set_veloctiy(int value) {
   veloctiy = value;
 }
-
+// set pins for motor
 void set_pins(int left_a, int left_b, int right_a, int right_b) {
   pinMode(left_a, OUTPUT);
   pinMode(left_b, OUTPUT);
