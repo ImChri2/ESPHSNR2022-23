0a87dc01f1b13814203467ddca4b3966c66ed42d
Lesh <11671272+derLesh@users.noreply.github.com>
2022-11-07 22:13:37 +0100

    Kommentare hinzugefügt

--- src/MotorControl.cpp
+++ src/MotorControl.cpp
@@ -2,6 +2,7 @@
 
 int veloctiy = 190;
 
+// Halte den Roboter an
 void motor_stop(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, 0);
   analogWrite(left_b, 0);
@@ -9,6 +10,7 @@ void motor_stop(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(right_b, 0);
 }
 
+// Bewege den Roboter nach links
 void motor_left(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, 0);
   analogWrite(left_b, veloctiy);
@@ -16,6 +18,7 @@ void motor_left(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(right_b, 0);
 }
 
+// Bewege den Roboter nach rechts
 void motor_right(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, veloctiy);
   analogWrite(left_b, 0);
@@ -23,6 +26,7 @@ void motor_right(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(right_b, veloctiy);
 }
 
+// Bewege den Roboter nach vorne
 void motor_forward(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(left_a, veloctiy);
   analogWrite(left_b, 0);
@@ -30,10 +34,12 @@ void motor_forward(int left_a, int left_b, int right_a, int right_b) {
   analogWrite(right_b, 0);
 }
 
+// Setze die Geschwindigkeit
 void set_veloctiy(int value) {
   veloctiy = value;
 }
 
+// Einlesen der Pins für die Motoren
 void set_pins(int left_a, int left_b, int right_a, int right_b) {
   pinMode(left_a, OUTPUT);
   pinMode(left_b, OUTPUT);
